660eab126c7c687cd5b94c8d429fea5c66eb36a1
Guille <guillermo@artvisual.net>
2012-11-12 12:53:33 +0100

    estilos

--- Controller/FeedsController.php
+++ Controller/FeedsController.php
@@ -91,6 +91,9 @@ class FeedsController extends AppController {
 				'order' => 'date desc'
 			));
 
+			App::import('Vendor', 'FunctionsVendor');
+			$functions = new FunctionsVendor();
+
 			$return = array();
 			foreach ($feeds as $post) {
 				//$return[] = $post['Post'];
@@ -99,7 +102,7 @@ class FeedsController extends AppController {
 					'id' => $Post['id'],
 					'blog_id' => $Post['blog_id'],
 					'title' => $Post['title'],
-					'date' => $this->timeago($Post['date']),
+					'date' => $functions->timeago($Post['date']),
 					'author' => $Post['author'],
 					'description' => $Post['description'],
 					'image' => $Post['image'],
@@ -382,77 +385,6 @@ class FeedsController extends AppController {
 			echo json_encode(array('status' => 'ko', 'message' => 'error en los datos')); die;
 		}
 
-	}
-
-	function timeago($datefrom, $dateto = -1) {
-		if ($datefrom <= 0) {
-			return "Hace mucho tiempo";
-		}
-		if($dateto==-1) {
-			$dateto = time();
-		}
-		$difference = $dateto - $datefrom;
-		if($difference < 60) {
-			$interval = "s";
-		}
-		elseif($difference >= 60 && $difference < 60*60) {
-			$interval = "n";
-		}
-		elseif($difference >= 60*60 && $difference < 60*60*24) {
-			$interval = "h";
-		}
-		elseif($difference >= 60*60*24 && $difference < 60*60*24*7) {
-			$interval = "d";
-		}
-		elseif($difference >= 60*60*24*7 && $difference < 60*60*24*30) {
-			$interval = "ww";
-		}
-		elseif($difference >= 60*60*24*30 && $difference < 60*60*24*365) {
-			$interval = "m";
-		}
-		elseif($difference >= 60*60*24*365) {
-			$interval = "y";
-		}
-		switch($interval) {
-			case "m":
-				$months_difference = floor($difference / 60 / 60 / 24 / 29);
-				while (mktime(date("H", $datefrom), date("i", $datefrom),
-							date("s", $datefrom), date("n", $datefrom) + ($months_difference),
-							date("j", $dateto), date("Y", $datefrom)) < $dateto) {
-					$months_difference++;
-				}
-				$datediff = $months_difference;
-				if($datediff == 12) {
-					$datediff--;
-				}
-				$res = ($datediff==1) ? "hace $datediff mes" : "hace $datediff meses";
-				break;
-			case "y":
-				$datediff = floor($difference / 60 / 60 / 24 / 365);
-				$res = ($datediff==1) ? "hace $datediff año" : "hace $datediff años";
-				break;
-			case "d":
-				$datediff = floor($difference / 60 / 60 / 24);
-				$res = ($datediff==1) ? "hace $datediff día" : "hace $datediff días";
-				break;
-			case "ww":
-				$datediff = floor($difference / 60 / 60 / 24 / 7);
-				$res = ($datediff==1) ? "la semana pasada" : "hace $datediff semanas";
-				break;
-			case "h":
-				$datediff = floor($difference / 60 / 60);
-				$res = ($datediff==1) ? "hace $datediff hora" : "hace $datediff horas";
-				break;
-			case "n":
-				$datediff = floor($difference / 60);
-				$res = ($datediff==1) ? "hace $datediff minuto" : "hace $datediff minutos";
-				break;
-			case "s":
-				$datediff = $difference;
-				$res = ($datediff==1) ? "hace $datediff segundo" : "hace $datediff segundos";
-				break;
-		}
-		return $res;
 	}
 
 	function arreglar() {
--- View/Posts/view.ctp
+++ View/Posts/view.ctp
@@ -1,3 +1,16 @@
+<?php
+App::import('Vendor', 'FunctionsVendor');
+$functions = new FunctionsVendor();
+?>
 <h4><?php echo $Post['title']; ?></h4>
+<blockquote>
+	<?php
+	if (!empty($Post['author'])) {
+		echo 'Por ' , $Post['author'] , ', ' , $functions->timeago($Post['date']);
+	} else {
+		echo ucfirst($functions->timeago($Post['date']));
+	}
+	?>
+</blockquote>
 <img src="<?php echo $Post['image']; ?>" class="image left" alt="<?php echo $Post['title']; ?>" />
 <p><?php echo nl2br($Post['description']); ?></p>
